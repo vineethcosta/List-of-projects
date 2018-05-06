5679dca14c1663409dd60c7a852c2b37ee969f54
vineethcosta <30414013+vineethcosta@users.noreply.github.com>
2018-05-06 12:57:15 +0530

    Update header.php

--- 2travellingladz/includes/header.php
+++ 2travellingladz/includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome To PHPLoversBlog</title>
+    <title>Welcome To 2TravellingLadz Blog</title>
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <!-- Custom styles for this template -->
