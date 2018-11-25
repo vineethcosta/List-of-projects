b13e370d0e861f1a451c2ef2510262a605f88d5f
Vineethcosta <reddyvineeth6@gmail.com>
2018-11-25 11:25:11 +0530

    Updates

---
+++ precache-manifest.8631636669ab9ba253bcae250b74583b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/educhampintern/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "381d92125ef995606b6a",
+    "url": "/educhampintern/static/js/runtime~main.381d9212.js"
+  },
+  {
+    "revision": "37ea92d2e8433a7e33cf",
+    "url": "/educhampintern/static/js/main.37ea92d2.chunk.js"
+  },
+  {
+    "revision": "b70ed63098de8a48d7bf",
+    "url": "/educhampintern/static/js/1.b70ed630.chunk.js"
+  },
+  {
+    "revision": "37ea92d2e8433a7e33cf",
+    "url": "/educhampintern/static/css/main.8dcfb6fd.chunk.css"
+  },
+  {
+    "revision": "91ae95c9e92ff6faa859418a1925511e",
+    "url": "/educhampintern/index.html"
+  }
+];
